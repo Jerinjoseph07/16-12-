a39fcda6a1d53bccf4903d68aa0abc17c5b543df
srutiap <135594255+srutiap@users.noreply.github.com>
2023-12-16 14:23:56 +0530

    first commit

--- src/component/Student/Students.jsx
+++ src/component/Student/Students.jsx
@@ -1,5 +1,5 @@
 import { Button, MenuItem, Select, TextField } from '@mui/material'
-import axios from 'axios';
+// import axios from 'axios';
 import React, { useState } from 'react'
 
 
@@ -10,20 +10,54 @@ const Students = () => {
           "Name":'',
           "Age":'12',
           "Course":'BCA',
-        });
+          });
+
+        var [selectedimage,setSelectedimage]=useState(null);
       
   const inputHandler =(event) =>{
     const {name,value}=event.target
     setInputs((inputs)=>({...inputs,[name]:value}))
     console.log(inputs)
   }
-  const addHandler=() =>{
-    axios.post("http://localhost:3005/new",inputs)
-    .then((Response)=>{
-      alert("record saved")
-    })
-      .catch(err=>console.log(err))
-    }
+  
+  const Handleimage=(event)=>{
+    const file=event.target.files[0];
+    setSelectedimage(file)
+    inputs.image1=file;
+
+  }
+  // const addHandler=() =>{
+    // axios.post("http://localhost:3005/new",inputs)
+    // .then((Response)=>{
+      // alert("record saved")
+    // })
+      // .catch(err=>console.log(err))
+    // }
+
+    const saveData=()=>{
+      const formdata= new FormData();
+      formdata.append('Admno',inputs.Admno);
+      formdata.append('Name',inputs.Name);
+      formdata.append('Age',inputs.Age);
+      formdata.append('Course',inputs.Course);
+      formdata.append('image1',selectedimage);
+      fetch('http://localhost:3005/new',{
+        method:'post',
+      body:formdata,
+      })
+    
+    .then((response)=>response.json())
+    .then((data)=>{
+
+          alert("Record saved")
+
+        })
+        .catch((err)=>{
+          console.log("error")
+        })
+      }
+    
+
   return (
     <div>
        <TextField  label="Admission Number" name="Admno" variant="outlined" value={inputs.Admno} onChange={inputHandler} /> <br />
@@ -35,7 +69,11 @@ const Students = () => {
     <MenuItem value="BCA">BCA</MenuItem>
     <MenuItem value="BCOM">Bcom</MenuItem>
   </Select>
-  <Button variant="contained" onClick={addHandler}>OK</Button>
+  <br></br>
+  <label>Choose file to upload</label>
+  <input type="file" onChange={Handleimage}></input>
+  <br></br>
+  <Button variant="contained" onClick={saveData}>OK</Button>
     </div>
   )
 }
